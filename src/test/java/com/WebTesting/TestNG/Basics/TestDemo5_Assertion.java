e3f66fe2ae6def046caad8946a7f77d40f9d5c15
darshan-ts <darshan.thippeswamy94@gmail.com>
2025-08-01 12:26:23 +0530

    01-08-2025

---
+++ src/test/java/com/WebTesting/TestNG/Basics/TestDemo5_Assertion.java
@@ -0,0 +1,49 @@
+package com.WebTesting.TestNG.Basics;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class TestDemo5_Assertion 
+{
+  @Test(enabled=false)
+  public void testHardAssertion() 
+  {
+	  
+	  String act="Selenium webdriver is webui automation api";
+	  String exp="Testng";
+	  //AssertionError
+	  //Assert.assertEquals(act,exp,"Test Fail: Strings are not equal!");
+	  
+	  //Assert.assertTrue(act.contains(exp),"Test Fail: String not match with partial data");
+	  
+	  //Scenario: Actual string should not contains TestNg
+	  
+	  Assert.assertFalse(act.contains(exp),"Test Fail: String match with partial data");
+	  System.out.println("Test Pass: String not match with partial data");
+	  System.out.println("---------------");
+	  System.out.println("---------------");
+	  System.out.println("---------------");
+	  
+  }
+  
+  @Test
+  public void testSoftAssertion()
+  {
+	  int a=100,b=204566777;
+	  	 
+	  SoftAssert sf=new SoftAssert();
+	  //sf.assertEquals(a,b,"Test Fail: Numbers are not equal");
+	  sf.assertTrue(a==b);
+	  System.out.println("Test Pass: Numbers are equal");
+	  System.out.println("---------------");
+	  System.out.println("---------------");
+	  System.out.println("---------------");
+	  
+	  sf.assertAll();
+	  
+  }
+  
+  
+  
+}
---
+++ src/test/java/com/WebTesting/TestNG/Basics/TestDemo6_HRMLoginValidation.java
@@ -0,0 +1,53 @@
+package com.WebTesting.TestNG.Basics;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo6_HRMLoginValidation {
+  @Test
+  public void testLogin()
+  {
+	  WebDriver driver=new ChromeDriver();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  
+	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	  
+	  //username
+	  driver.findElement(By.name("username")).sendKeys("Admin");
+	  
+	  //password
+	  driver.findElement(By.name("password")).sendKeys("admin1234");
+	  
+	  
+	  //login
+	  driver.findElement(By.tagName("button")).click();
+	  //navigate to next page
+	  
+	  //current url validation
+	  String actUrl=driver.getCurrentUrl();
+	  String expUrl="dashboard";
+	  
+	  Assert.assertTrue(actUrl.contains(expUrl),"Login Fail");
+	  System.out.println("Login Completed!");
+	  
+	  //logout
+	  driver.findElement(By.xpath("//i[contains(@class,'oxd-userdropdown-icon')]")).click();
+	  driver.findElement(By.linkText("Logout")).click();
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+  }
+}
